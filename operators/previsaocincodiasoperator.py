e3ebd5d8933494339630b87f0694e5330be6844d
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-11-09 20:28:33 -0300

    classe

---
+++ operators/previsaocincodiasoperator.py
@@ -0,0 +1,28 @@
+from typing import Dict
+from hooks.openweaterhook import OpenWeatherHook
+from operators.openweatheroperator import OpenWeatherOperator
+from src.dados.iinfra_dados import IinfraDados
+
+
+class PrevisaoCincoDiasOperator(OpenWeatherOperator):
+
+    template_fields = [
+        'camada_data_lake',
+        'path_extracao',
+        'municipio',
+        'metricas',
+        'caminho_save_arquivos'
+        'extracao'
+    ]
+
+    def __init__(
+            self, camada_data_lake: str, path_extracao: str,
+            municipio: str, metricas: str,
+            caminho_save_arquivos: IinfraDados, extracao: OpenWeatherHook, **kwargs
+    ):
+        super().__init__(camada_data_lake, path_extracao, municipio,
+                         metricas, caminho_save_arquivos, extracao, **kwargs)
+
+    def execute(self, context):
+        for json_reponse in self._extracao.run():
+            self.gravar_dados(json_reponse)
